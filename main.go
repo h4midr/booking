82bb7bcef036d20bc0540a8a2e8e9489cfecdb15
h4midr <hassaniyaqoti@gmail.com>
2021-04-09 15:07:30 +0430

    some example of implementation

--- main.go
+++ main.go
@@ -1,1 +1,57 @@
 package main
+
+import (
+	"log"
+
+	"github.com/h4midr/booking/models"
+)
+
+func main() {
+	o, err := models.NewOffice("7:30", "22:00")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	ses, err := models.NewSession("9:30", "10:30")
+	if err != nil {
+		log.Panic(err)
+	}
+	{
+		bk, err := o.IsBookable(ses)
+		log.Println(err)
+		log.Printf("Ses %#v is Bookable : %v\n", ses, bk)
+
+	}
+	s, err := o.Book(ses)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println("Session 1 Booked")
+	// log.Printf("Now the Office is %#v\n", o)
+
+	ses2, err := models.NewSession("8:30", "9:30")
+	if err != nil {
+		log.Panic(err)
+	}
+	if bk, err := o.IsBookable(ses2); bk {
+		log.Println("Session 2 is Bookable")
+	} else {
+		log.Println(err)
+		log.Println("Session 2 is not Bookable")
+	}
+
+	_, err = o.Book(ses2)
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.Println("Session 2 Booked")
+	}
+
+	//
+	err = o.UnBook(s.ID)
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println("Session1 Unbooked")
+
+}
